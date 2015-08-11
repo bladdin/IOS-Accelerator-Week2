40c5613403ddbe1fd44f423bc430ad8d198563e3
Benjamin Laddin <benjamin.laddin@gmail.com>
2015-08-11 12:01:09 -0700

    hw 1 upload 1

---
+++ parse-starter-project-1.7.5 (1)/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var alertButton: UIButton!
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  let picker : UIImagePickerController = UIImagePickerController()
+  
+  
+  let alert = UIAlertController(title: "Button Clicked", message: "Yes the button was clicked", preferredStyle: UIAlertControllerStyle.Alert)
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      
+      let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (alert) -> Void in
+        println("Alert Canceled")
+      }
+      
+      let destroyAction = UIAlertAction(title: "Destroy", style: .Destructive) { (alert) -> Void in
+        println("Alert destroyed")
+      }
+     
+    let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.Default) { (alert) -> Void in
+        println("Confirm")
+      }
+      
+    let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      self.presentViewController(self.picker, animated: true, completion: nil)
+      
+    }
+      
+    let colorInvertAction = UIAlertAction(title: "Color Invert", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      let image = CIImage(image: self.imageView.image)
+      let invertFiter = CIFilter(name: "CIColorInvert")
+      invertFiter.setValue(image, forKey: kCIInputImageKey)
+      
+      let context = CIContext(options: nil)
+      
+      let options = [kCIContextWorkingColorSpace : NSNull()]
+      let eaglContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
+      let gpuContext = CIContext(EAGLContext: eaglContext, options: options)
+      
+      let outputImage = invertFiter.outputImage
+      let extent = outputImage.extent()
+      let cgImage = gpuContext.createCGImage(outputImage, fromRect: extent)
+      let finalImage = UIImage(CGImage: cgImage)
+      self.imageView.image = finalImage
+    }
+      
+     let sepiaAction = UIAlertAction(title: "Sepia", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      let image = CIImage(image: self.imageView.image)
+      let sepiaFilter = CIFilter(name: "CIPhotoEffectInstant")
+      sepiaFilter.setValue(image, forKey: kCIInputImageKey)
+      
+      let context = CIContext(options: nil)
+      
+      let options = [kCIContextWorkingColorSpace : NSNull()]
+      let eaglContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
+      let gpuContext = CIContext(EAGLContext: eaglContext, options: options)
+      
+      let outputImage = sepiaFilter.outputImage
+      let extent = outputImage.extent()
+      let cgImage = gpuContext.createCGImage(outputImage, fromRect: extent)
+      let finalImage = UIImage(CGImage: cgImage)
+      self.imageView.image = finalImage
+      }
+    
+    let colorMapAction = UIAlertAction(title: "Cosmic Effect", style: UIAlertActionStyle.Default) { (alert) -> Void in
+      let image = CIImage(image: self.imageView.image)
+      let heatMapFilter = CIFilter(name: "CITwirlDistortion")
+      heatMapFilter.setValue(image, forKey: kCIInputImageKey)
+      
+      let context = CIContext(options: nil)
+      
+      let options = [kCIContextWorkingColorSpace : NSNull()]
+      let eaglContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
+      let gpuContext = CIContext(EAGLContext: eaglContext, options: options)
+      
+      let outputImage = heatMapFilter.outputImage
+      let extent = outputImage.extent()
+      let cgImage = gpuContext.createCGImage(outputImage, fromRect: extent)
+      let finalImage = UIImage(CGImage: cgImage)
+      self.imageView.image = finalImage
+    }
+      
+      alert.addAction(cameraAction)
+      alert.addAction(colorInvertAction)
+      alert.addAction(sepiaAction)
+      alert.addAction(colorMapAction)
+      
+      
+      self.picker.delegate = self
+      self.picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+      
+      
+      
+      
+      
+      //if let popover = self.popoverPresentationController
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  @IBAction func buttonPressed(sender: AnyObject) {
+    
+//    alert.modalPresentationStyle = UIModalPresentationStyle.Popover
+//    
+//    
+//    if let popover = alert.popoverPresentationController{
+//      popover.sourceView = view
+//      popover.sourceRect = alertButton.frame
+//      
+//    }
+
+    self.presentViewController(alert, animated: true, completion : nil)
+    
+  }
+}
+
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[NSObject : AnyObject]) {
+    let image: UIImage = (info[UIImagePickerControllerOriginalImage] as? UIImage)!
+    self.imageView.image = image
+    self.picker.dismissViewControllerAnimated(true, completion: nil)
+    
+    
+  }
+  
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    self.picker.dismissViewControllerAnimated(true, completion: nil)
+    println("picker canceled")
+  }
+  
+  
+  
+  
+}
+
